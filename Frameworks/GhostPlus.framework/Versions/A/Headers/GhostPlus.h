34b3a046a533de798b890b2f4c2957fa376e1f37
VANSTUDIO <vanstudio@daum.net>
2018-05-11 13:45:37 +0900

    Release 2.21 - Rechability class removed.

--- Frameworks/GhostPlus.framework/Versions/A/Headers/GhostPlus.h
+++ Frameworks/GhostPlus.framework/Versions/A/Headers/GhostPlus.h
@@ -53,8 +53,8 @@
 #import <AFNetworking/AFNetworking.h>
 
 // Constans
-#define GHOSTPLUS_VERSION           @"2.20";
-#define GHOSTPLUS_BUILD_VERSION     @"220";
+#define GHOSTPLUS_VERSION           @"2.21";
+#define GHOSTPLUS_BUILD_VERSION     @"221";
 
 /**
  GhostPlus 메인클래스
--- GhostPlus.podspec
+++ GhostPlus.podspec
@@ -1,4 +1,4 @@
-@version = "2.20"
+@version = "2.21"
 Pod::Spec.new do |s|
   s.name             = 'GhostPlus'
   s.version          = @version
